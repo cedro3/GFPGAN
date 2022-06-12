be079c5cd60d8492192d65df57e4dc6937ddc79f
Yuichi Sugita <34782651+cedro3@users.noreply.github.com>
2022-06-12 10:06:46 +0900

    Add files via upload

---
+++ function.py
@@ -0,0 +1,67 @@
+# --- display_mp4 ---
+from IPython.display import display, HTML
+
+def display_mp4(path):
+    from base64 import b64encode
+    mp4 = open(path,'rb').read()
+    data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
+    display(HTML("""
+    <video width=700 controls>
+        <source src="%s" type="video/mp4">
+    </video>
+    """ % data_url))
+
+
+# --- display_pic ---
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import os
+import glob
+
+def display_pic(folder):
+    fig = plt.figure(figsize=(30, 60))
+    files = sorted(glob.glob(folder+'/*.jpg'))
+    for i, file in enumerate(files):
+        img = Image.open(file)    
+        images = np.asarray(img)
+        ax = fig.add_subplot(10, 5, i+1, xticks=[], yticks=[])
+        image_plt = np.array(images)
+        ax.imshow(image_plt)
+        name = os.path.basename(file)
+        ax.set_xlabel(name, fontsize=30)
+        fig.tight_layout()
+    plt.show()
+    plt.close()
+
+
+# --- reset_folder ---
+import shutil
+
+def reset_folder(path):
+    if os.path.isdir(path):
+      shutil.rmtree(path)
+    os.makedirs(path,exist_ok=True)
+    
+    
+# --- size adjust --- 
+import cv2
+
+def size_opt(file):
+    max_size = 1024
+    npyImage = cv2.imread(filename = file, flags = cv2.IMREAD_COLOR)
+    intWidth = npyImage.shape[1]
+    intHeight = npyImage.shape[0]
+
+    fltRatio = float(intWidth) / float(intHeight)
+    intWidth = min(int(max_size * fltRatio), max_size)
+    intHeight = min(int(max_size / fltRatio), max_size)
+
+    # make even
+    if not intWidth % 2 == 0:
+      intWidth +=1
+    if not intHeight % 2 ==0:
+      intHeight +=1
+
+    npyImage = cv2.resize(src=npyImage, dsize=(intWidth, intHeight), fx=0.0, fy=0.0, interpolation=cv2.INTER_AREA)
+    cv2.imwrite(file, npyImage)
